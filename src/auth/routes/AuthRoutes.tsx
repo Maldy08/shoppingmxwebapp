f08a43f9d06e44947358f66bea4e81fe151a9a64
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-05-11 16:59:29 -0700

    nada

--- src/auth/routes/AuthRoutes.tsx
+++ src/auth/routes/AuthRoutes.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../pages/LoginPage"
 import { RegisterPage } from "../pages/RegisterPage"
+import Example from "../pages/Example"
 
 export const AuthRoutes = () => {
     return(
         <Routes>
             <Route path="login" element={ <LoginPage/> } />
             <Route path="register" element={ <RegisterPage/> } />
+            <Route path="example" element={ <Example/> } />
             <Route path="/*" element={ <Navigate to="/auth/login" />} />
         </Routes>
     )
